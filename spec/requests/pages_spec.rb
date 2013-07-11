462e07c575bb59d747cbfce3d614367388212e0b
Matt Rubin <mattrubin@me.com>
2013-07-10 21:11:06 -0700

    Added full_title helper method

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -5,19 +5,23 @@ describe "Static pages" do
   let(:base_title) { "Primer" }
 
   describe "Home page" do
-  	before { visit '/pages/home' }
+    before { visit '/pages/home' }
 
     it "should have the content 'Primer'" do
       expect(page).to have_content('Primer')
     end
 
-    it "should have the title 'Home'" do
-      expect(page).to have_title("#{base_title} | Home")
+    it "should have the base title" do
+      expect(page).to have_title(base_title)
+    end
+
+    it "should not have a custom page title" do
+      expect(page).not_to have_title('| Home')
     end
   end
 
   describe "Help page" do
-  	before { visit '/pages/help' }
+    before { visit '/pages/help' }
 
     it "should have the content 'Help'" do
       expect(page).to have_content('Help')
@@ -29,7 +33,7 @@ describe "Static pages" do
   end
 
   describe "About page" do
-  	before { visit '/pages/about' }
+    before { visit '/pages/about' }
 
     it "should have the content 'About'" do
       expect(page).to have_content('About')
@@ -41,7 +45,7 @@ describe "Static pages" do
   end
 
   describe "Contact page" do
-  	before { visit '/pages/contact' }
+    before { visit '/pages/contact' }
 
     it "should have the content 'Contact'" do
       expect(page).to have_content('Contact')
