de04be798a780507c1e6f9b2e30215e2513139e1
Matt Rubin <mattrubin@me.com>
2013-07-10 20:13:47 -0700

    Added page titles

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -8,6 +8,11 @@ describe "Static pages" do
       visit '/pages/home'
       expect(page).to have_content('Primer')
     end
+
+    it "should have the title 'Home'" do
+      visit '/pages/home'
+      expect(page).to have_title("Primer | Home")
+    end
   end
 
   describe "Help page" do
@@ -16,6 +21,11 @@ describe "Static pages" do
       visit '/pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the title 'Help'" do
+      visit '/pages/help'
+      expect(page).to have_title("Primer | Help")
+    end
   end
 
   describe "About page" do
@@ -23,6 +33,11 @@ describe "Static pages" do
     it "should have the content 'About'" do
       visit '/pages/about'
       expect(page).to have_content('About')
+    end
+
+    it "should have the title 'About'" do
+      visit '/pages/about'
+      expect(page).to have_title("Primer | About")
     end
   end
 end
