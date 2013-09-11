87cf46c7e3e79b5630491005ee79004205953d88
Matt Rubin <mattrubin@me.com>
2013-09-11 00:47:20 -0400

    Specs for more join models

--- spec/models/dependency_spec.rb
+++ spec/models/dependency_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Dependency do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @dependency = Dependency.new()
+    @dependency.prereq = Concept.new(name: "Addition")
+    @dependency.postreq = Concept.new(name: "Subtraction")
+  end
+
+  subject { @dependency }
+
+  it { should respond_to :prereq }
+  it { should respond_to :postreq }
+
+  it { should be_valid }
+
+  describe "without a prereq" do
+    before { @dependency.prereq = nil }
+    it { should_not be_valid }
+  end
+
+  describe "without a postreq" do
+    before { @dependency.postreq = nil }
+    it { should_not be_valid }
+  end
 end
