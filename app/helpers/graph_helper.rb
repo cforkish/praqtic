c85c79353786796ac8ccb70dc6f626c842b31e03
Charlie Forkish <cforkish@gmail.com>
2013-09-14 02:56:18 -0700

    adding graph_helper to last commit

--- app/helpers/graph_helper.rb
+++ app/helpers/graph_helper.rb
@@ -3,6 +3,7 @@ module GraphHelper
   def category_to_node(category)
     { "name" => category.name,
       "url" => category_path(category.id),
+      "category_id" =>category.id,
       "children"   => category.children.map { |c| category_to_node(c) } # uncomment to add concept leafs # + category.concepts.map { |c| concept_to_node(c) }
     }
   end
