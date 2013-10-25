9e18ebc01e76ca0f54df22f53c22551a9e3af0da
Charlie Forkish <cforkish@gmail.com>
2013-10-24 18:09:43 -0700

    better formatting for adding lessons in question form

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,7 +4,7 @@ class Lesson < ActiveRecord::Base
 
   has_many :interactions, :class_name => "LessonInteraction"
   has_many :users, :through => :interactions
-  has_and_belongs_to_many :question_answers
+  has_and_belongs_to_many :questions
 
   validates :name,  presence: true, length: { maximum: 50 }
   validates_presence_of :quiz
